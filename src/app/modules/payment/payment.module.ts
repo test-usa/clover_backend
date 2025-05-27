96d5f2965e7be2e04e6118368511f4f2130c0653
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2025-05-28 02:06:11 +0600

    Defined CreatePaymentInput TypeScript interface for creating payments

--- src/app/modules/payment/payment.module.ts
+++ src/app/modules/payment/payment.module.ts
@@ -1,17 +1,24 @@
 import mongoose, { Schema, Document, Model, model } from 'mongoose';
 import { TStripePaymentIntent } from './payment.interface';
+import { PaymentStatus } from './payment.constant';
+
 
 
 const StripePaymentIntentSchema: Schema = new Schema<TStripePaymentIntent>({
   tranctionId: { type: String, required: true },
   amount: { type: Number, required: true },
   currency: { type: String, required: true },
-  status: { type: String, required: true },
+  status: {
+    type: String,
+    enum: Object.values(PaymentStatus),
+    required: true,
+  },
   client_secret: { type: String, required: true },
   created: { type: Number, required: true },
   capture_method: { type: String, required: true },
   payment_method_types: { type: [String], required: true },
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  swapId: { type: String, required: true },
 });
 
 
