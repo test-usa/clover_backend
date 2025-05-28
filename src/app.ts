0491d9965f61e9caa83f7ba7ff14d78e7ee9fa1e
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-05-29 01:30:47 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,43 @@
+// import express, { Request, Response } from "express";
+// import cors from "cors";
+// import router from "./app/routes";
+// import globalErrorHandler from "./app/middleWear/globalErrorHandler";
+// import notFound from "./app/middleWear/notFound";
+// import cookieParser from "cookie-parser";
+// import swaggerUi from 'swagger-ui-express';
+// import { paymentWebhook } from "./app/modules/payment/payment.route";
+// import { swaggerSpec } from "./app/docs";
+
+// const app = express();
+
+
+// // Middlewares
+// app.use(cors({
+//   origin: '*', // allow all origins
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // optional: specify allowed methods
+//   allowedHeaders: ['Content-Type', 'Authorization'], // optional: specify allowed headers
+// }));
+// app.use('/api/v1/payment/',paymentWebhook)
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use("/api/v1", router);
+// // Serve Swagger docs
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Hello Legalmate!");
+// });
+
+
+// app.use(notFound);
+// app.use(globalErrorHandler);
+
+
+// export default app;
+
+
+
+
 import express, { Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
@@ -10,27 +50,30 @@ import { swaggerSpec } from "./app/docs";
 
 const app = express();
 
-
-// Middlewares
+// CORS + Body Parser
 app.use(cors({
-  origin: '*', // allow all origins
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // optional: specify allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // optional: specify allowed headers
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-app.use('/api/v1/payment/',paymentWebhook)
+app.options('*', cors()); // preflight requests
+
 app.use(express.json());
 app.use(cookieParser());
+
+// Routes
+app.use('/api/v1/payment/', paymentWebhook);
 app.use("/api/v1", router);
-// Serve Swagger docs
+
+// Swagger Docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// Test route
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Legalmate!");
 });
 
-
 app.use(notFound);
 app.use(globalErrorHandler);
-
 
 export default app;
