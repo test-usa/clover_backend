498c05f91b2c8c1947187ec748bc741d90feb732
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-05-29 04:26:54 +0600

    issue resolved

--- src/app.ts
+++ src/app.ts
@@ -52,7 +52,14 @@ const allowedOrigins = [
 // CORS + Body Parser
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
